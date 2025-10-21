a899580e4e50b9f52b301fee9509f697162d8be7
OhradkaGergo <ohradkagergo@turr.hu>
2025-10-21 12:53:17 +0200

    órai munka 6

--- src/app/components/customers/list/list.component.ts
+++ src/app/components/customers/list/list.component.ts
@@ -1,13 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ApiService } from '../../../services/api.service';
+import { Traffic } from '../../../interfaces/traffic';
+import { ApiRes } from '../../../interfaces/apires';
 
 @Component({
   selector: 'app-customerslist',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class CustomersListComponent {
-
+  constructor(private api:ApiService){}
+  
+  traffic: Traffic[] = []
+  
+  async ngOnInit() {
+    this.getAllTraffic()
+  }
+  
+  getAllTraffic() {
+    this.api.selectAll('traffic').then((res: ApiRes) => {
+      if (res.status == 200) {
+        this.traffic = res.data
+      } else {
+        alert(res.message)
+      }
+    })
+  }
 }
